49c82ffbcbc5c22733ac064bc8cf2fd3c5e2a8fe
DanielSedell02 <danne.sedell@gmail.com>
2023-12-15 19:45:01 +0100

    createButton och selectedButton const

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
-// Globala variabler
+const WORDS = ['banana', 'apple', 'watermelon', 'honeydew', 'orange', 'pineapple']
+let answer;
+let wordState;
+let num;
 
-const wordList;      // Array: med spelets alla ord
-let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+const createButton = () => {
+    const start = 'a'
+    const end = 'z'
+    for (let i = start.charCodeAt(); i <= end.charCodeAt(); i++) {
+        let btn = document.createElement('button')
+ $(btn).addClass('btn btn-primary m-1')
+        $(btn).one('click',selectedButton) // only call function once
+        $(btn).text(String.fromCharCode(i)) // convert from charcode to letter, then insert into button
+        $('#button-container').append(btn)
+    }
+}
 
-let guesses = 0;     // Number: håller antalet gissningar som gjorts
-let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
-
-let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
-let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
-let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
-
-// Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-// Funktion som slumpar fram ett ord
-// Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
-// Funktion som körs när du trycker på bokstäverna och gissar bokstav
-// Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
-// Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
-
+const selectedButton = (e) => {
+    $(e.target).addClass('btn-secondary') // change the button background color to gray 
+    let guessLetter = $(e.target).text() // get the text of button selected
+    // loop through each letter in answer
+    for (let i = 0; i < answer.length; i++){
+        // if the selected letter is correct
+        if ( guessLetter === answer[i] ){
+            let correctEle = $('#word-container .btn-warning')[i] // get the correct letter button element with index
+            $(correctEle).text(guessLetter) 
+        }
+    }
+   
