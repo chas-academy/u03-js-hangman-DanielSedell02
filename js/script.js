b61b2b629d012584cb46e80e5dd3f8b9f32b60c2
DanielSedell02 <danne.sedell@gmail.com>
2024-02-24 19:24:35 +0100

    added function that draws out the images of the stickman

--- js/script.js
+++ js/script.js
@@ -2,91 +2,96 @@ const WORDS = ['garage', 'berry', 'order', 'suffering', 'exact', 'doubt', 'offen
 let answer;
 let wordState;
 let num;
-
+let incorrectGuesses = 0;
 
 const createButton = () => {
-  const start = 'a';
-  const end = 'z';
-  for (let i = start.charCodeAt(); i <= end.charCodeAt(); i++) {
-    const btn = document.createElement('button');
-    $(btn).addClass('btn btn-primary m-1');
-    $(btn).one('click', selectedButton); // only call function once
-    $(btn).text(String.fromCharCode(i)); // convert from charcode to letter, then insert into button
-    $('#button-container').append(btn);
-  }
+    const start = 'a';
+    const end = 'z';
+    for (let i = start.charCodeAt(); i <= end.charCodeAt(); i++) {
+        const btn = document.createElement('button');
+        $(btn).addClass('btn btn-primary m-1');
+        $(btn).one('click', selectedButton); // only call function once
+        $(btn).text(String.fromCharCode(i)); // convert from charcode to letter, then insert into button
+        $('#button-container').append(btn);
+    }
 };
 
-
 const selectedButton = (e) => {
-  const guessLetter = $(e.target).text(); // get the text of button selected
-  // loop through each letter in answer
-  for (let i = 0; i < answer.length; i++) {
-    // if the selected letter is correct
-    if (guessLetter === answer[i]) {
-      const correctEle = $('#word-container .btn-warning')[i]; // get the correct letter button element with index
-      $(correctEle).text(guessLetter);
+    const guessLetter = $(e.target).text(); // get the text of button selected
+    // loop through each letter in answer
+    let correctGuess = false;
+    for (let i = 0; i < answer.length; i++) {
+        // if the selected letter is correct
+        if (guessLetter === answer[i]) {
+            const correctEle = $('#word-container .btn-warning')[i]; // get the correct letter button element with index
+            $(correctEle).text(guessLetter);
+            correctGuess = true;
+        }
     }
-  }
-  num--;
-  $('#guesses').text(num); // update number of guess in html
+    if (!correctGuess) {
+        incorrectGuesses++;
+        $('#hangman-image').attr('src', `images/h${incorrectGuesses}.png`);
+    }
+    num--;
+    $('#guesses').text(num); // update number of guess in html
 
-  if (checkWin()) {
-    setTimeout(() => {
-      alert('Awesome! You\'re correct!');
-      endGame();
-    }, 100);
-  } else if (num === 0) {
-    setTimeout(() => {
-      alert('Game over!');
-      endGame();
-    }, 100);
-  }
+    if (checkWin()) {
+        setTimeout(() => {
+            alert('Awesome! You\'re correct!');
+            endGame();
+        }, 100);
+    } else if (num === 0) {
+        setTimeout(() => {
+            alert('Game over!');
+            endGame();
+        }, 100);
+    }
 };
 
 const endGame = () => {
-  // create a restart button
-  const restartButton = document.createElement('button');
-  $(restartButton).addClass('btn btn-danger');
-  $(restartButton).text('Play again');
-  $(restartButton).one('click', () => {
-    startGame();
-    $(restartButton).remove(); // remove button when clicked
-  });
-  $('#board').append(restartButton);
+    // create a restart button
+    const restartButton = document.createElement('button');
+    $(restartButton).addClass('btn btn-danger');
+    $(restartButton).text('Play again');
+    $(restartButton).one('click', () => {
+        startGame();
+        $(restartButton).remove(); // remove button when clicked
+    });
+    $('#board').append(restartButton);
 };
 
 const checkWin = () => {
-  let win = false;
-  if (!wordState.includes('_')) {
-    win = true;
-  }
-  return win;
+    let win = false;
+    if (!wordState.includes('_')) {
+        win = true;
+    }
+    return win;
 };
+
 const showWordState = () => {
-  for (const letter of answer) {
-    // button for each letter of answer
-    const btn = document.createElement('button');
-    $(btn).addClass('btn btn-warning m-1');
-    $(btn).text('_');
-    $('#word-container').append(btn);
-    wordState.push('_'); // create an array to check the word state
-  }
+    for (const letter of answer) {
+        // button for each letter of answer
+        const btn = document.createElement('button');
+        $(btn).addClass('btn btn-warning m-1');
+        $(btn).text('_');
+        $('#word-container').append(btn);
+        wordState.push('_'); // create an array to check the word state
+    }
 };
 
 const startGame = () => {
-  // state of variables
-  answer = WORDS[Math.floor(Math.random() * WORDS.length)];
-  wordState = [];
-  num = 6;
-  $('#guesses').text(num);
-  $('#button-container').html('');
-  $('#word-container').html('');
-  createButton();
-  showWordState();
+    // state of variables
+    answer = WORDS[Math.floor(Math.random() * WORDS.length)];
+    wordState = [];
+    num = 6;
+    incorrectGuesses = 0;
+    $('#guesses').text(num);
+    $('#button-container').html('');
+    $('#word-container').html('');
+    $('#hangman-container').html('<img id="hangman-image" src="images/h0.png" alt="Hangman Image">');
+    createButton();
+    showWordState();
 };
 
 startGame();
-
-
-
 
